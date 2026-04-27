e985c82f31a061a3c075b4895ca35e6415d0ce74
Jabale Nur Adnan <your_email@example.com>
2026-04-27 23:30:21 +0600

    add creation and update timestamps to User entity

--- src/main/java/com/project/fitnessmonolith/model/User.java
+++ src/main/java/com/project/fitnessmonolith/model/User.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,7 +25,10 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
