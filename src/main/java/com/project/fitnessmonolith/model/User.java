625a197e11f57b4096d4c598187602b716cc4b3e
Jabale Nur Adnan <your_email@example.com>
2026-04-27 23:24:50 +0600

    refactor User and UserService to use Builder pattern for user creation

--- src/main/java/com/project/fitnessmonolith/model/User.java
+++ src/main/java/com/project/fitnessmonolith/model/User.java
@@ -2,6 +2,7 @@ package com.project.fitnessmonolith.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
--- src/main/java/com/project/fitnessmonolith/service/UserService.java
+++ src/main/java/com/project/fitnessmonolith/service/UserService.java
@@ -4,12 +4,9 @@ import com.project.fitnessmonolith.dto.RegisterRequest;
 import com.project.fitnessmonolith.dto.UserResponse;
 import com.project.fitnessmonolith.model.User;
 import com.project.fitnessmonolith.repository.UserRepository;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,17 +15,23 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserResponse register(RegisterRequest request) {
-        User user = new User(
-                null,
-                request.getEmail(),
-                request.getPassword(),
-                request.getFirstName(),
-                request.getLastName(),
-                Instant.parse("2024-01-01T00:00:00Z").atZone(ZoneOffset.UTC).toLocalDateTime(),
-                Instant.parse("2024-01-01T00:00:00Z").atZone(ZoneOffset.UTC).toLocalDateTime(),
-                List.of(),
-                List.of()
-        );
+            User user = User.builder()
+                    .email(request.getEmail())
+                    .password(request.getPassword())
+                    .firstName(request.getFirstName())
+                    .lastName(request.getLastName())
+                    .build();
+//        User user = new User(
+//                null,
+//                request.getEmail(),
+//                request.getPassword(),
+//                request.getFirstName(),
+//                request.getLastName(),
+//                Instant.parse("2024-01-01T00:00:00Z").atZone(ZoneOffset.UTC).toLocalDateTime(),
+//                Instant.parse("2024-01-01T00:00:00Z").atZone(ZoneOffset.UTC).toLocalDateTime(),
+//                List.of(),
+//                List.of()
+//        );
 
         User savedUser = userRepository.save(user);
         return mapToResponse(savedUser);
