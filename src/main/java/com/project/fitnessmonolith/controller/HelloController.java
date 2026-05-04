cf46d7bdfea2ff8cb524a8b5822532a8b337dc78
Jabale Nur Adnan <your_email@example.com>
2026-05-04 22:58:30 +0600

    Role Based Access control -- Spring Security

---
+++ src/main/java/com/project/fitnessmonolith/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.project.fitnessmonolith.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/admin/hello")
+    public String sayAdminHello(){
+        return "Hello Admin";
+    }
+
+    @GetMapping("/user/hello")
+    public String sayUserHello(){
+        return "Hello User";
+    }
+}
